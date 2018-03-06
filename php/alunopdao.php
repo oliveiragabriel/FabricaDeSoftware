8800a0924fcc7d6015ce473d8247f457420fde44
miguelmicael <fragadasilvamicael@yahoo.com.br>
2018-03-06 15:25:17 -0300

    Add files via upload

---
+++ php/alunopdao.php
@@ -0,0 +1,17 @@
+<?php
+$conexao = null;
+
+function conexao(&$conexao){
+    $conexao = mysqli_connect("localhost", "root", "", "dbceli");
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+};
+
+function insert($nome, $documento, $telefone, $email, $ie, $idcurso){
+    conexao($conexao);
+    $query = mysqli_query($conexao, "INSERT INTO aluno(nome,documento,telefone, email, ie, idcurso) 
+  VALUES('$nome', '$documento' ,  '$telefone', '$email', '$ie' , '$idcurso')") or die("Erro ao tentar cadastrar");
+    
+}; 
---
+++ php/testeaddescricao.php
@@ -0,0 +1,3 @@
+<?php
+require_once 'alunopdao.php';
+insert('ana', '55555', '278888', 'ana@gmail', 'i', '1');
