af7a04f43758aafcc5f0cb9cbb5df3b67a11b76c
jvjasam <josevictorjasam@yahoo.com.br>
2018-03-08 17:13:42 -0300

    Update extrairvalidar.php

--- php/extrairvalidar.php
+++ php/extrairvalidar.php
@@ -5,37 +5,121 @@
    $documento=$_POST['document'];
    $curso= $_POST['course'];
    $telefone=$_POST['phone'];
-   $email=$_POST['mail'];
-   $ie=$_POST['checkbox'];
+   $email=$_POST['email'];
+   $ie=$_POST['radio'];
    
    validar($nome, $documento, $curso, $telefone, $email, $ie);
  
    function validarnome($nome){
        if( trim($nome)=="" ){
-           echo "ERRO";
+           echo "ERRO vazio nome";
        }
-       
-       
-      $arraynome= str_split($nome);
-      $lengthnome= strlen($nome);
-      $erroletra=0;
-      for($i=0;$i<$lengthnome;$i++){
-          if(is_numeric ($arraynome[$i])){
-             $erroletra++;
-          }
+       else{
+            $arraynome= str_split($nome);
+            $lengthnome= strlen($nome);
+            $erroespecial=0;
+            $erroletra=0;
+            for($i=0;$i<$lengthnome;$i++){
+                if(is_numeric ($arraynome[$i])){
+                    $erroletra=1;
+                }
+            }
+            for($i=0;$i<$lengthnome;$i++){
+                $caracasciicode=ord($arraynome[$i]);
+                if($caracasciicode==32 || ($caracasciicode>=65 && $caracasciicode<=90) || ($caracasciicode>=97 && $caracasciicode<=122) || ($caracasciicode>=128 && $caracasciicode<=155) || $caracasciicode>=157 || ($caracasciicode>=160 && $caracasciicode<=165)){
+                    
+                }
+                    else{
+                           $erroespecial=1;                 
+                    }
+                }
+                
+                if($erroletra==1){
+                    echo "Erro número  ";
+                }
+                if($erroespecial==1){
+                    echo "Caractere inválido nome";
+                }
+            }
             
-          
-          
-        }
-            if($erroletra>0){
-              echo "Erro letra  ";
-      }
+       
    };
- 
+   function validardocumento($documento){
+       if( trim($documento)=="" ){
+           echo "Campo documento está vazio";
+       }
+       else{
+           $arraydocumento= str_split($documento);
+           $lengthdocumento= strlen($documento);
+           $erroespecial=0;
+           for($i=0;$i<$lengthdocumento;$i++){
+               $caracasciicode=ord($arraydocumento[$i]);
+               if($caracasciicode==32 || ($caracasciicode>=45 && $caracasciicode<=46) || ($caracasciicode>=65 && $caracasciicode<=90) || ($caracasciicode>=97 && $caracasciicode<=122) || ($caracasciicode>=128 && $caracasciicode<=155) || $caracasciicode>=157 || ($caracasciicode>=160 && $caracasciicode<=165)){
+                   
+               }
+               else{
+                   $erroespecial=1;
+               }
+           }
+           if($erroespecial==1){
+               echo "Caractere inválido documento";
+           }
+       }
+   };
+   
+   function validartelefone($telefone){
+       if( trim($telefone)=="" ){
+           echo "ERRO vazio telefone";
+       }
+           else{
+               $arraytelefone= str_split($telefone);
+               $lengthtelefone= strlen($telefone);
+               $erroespecial=0;
+               for($i=0;$i<$lengthtelefone;$i++){
+                   $caracasciicode=ord($arraytelefone[$i]);
+                   if($caracasciicode==45 || $caracasciicode==32 || $caracasciicode>=40 && $caracasciicode<=41 ||$caracasciicode>=48 && $caracasciicode<=57){
+                       
+                   }
+                   else{
+                       $erroespecial=1;
+                   }
+               
+               }
+               if($erroespecial==1){
+                   echo "Caractere inválido telefone";
+               }
+           }
+   };
+   
+   function validaremail($email){
+       if( trim($email)=="" ){
+           echo "ERRO vazio email";
+       }
+       else{
+           $arrayemail= str_split($email);
+           $lengthemail= strlen($email);
+           
+           
+       }
+   };
+   
+   function validarie ($ie){
+       if(!isset($ie)){
+
+        echo "ERRO vazio checkbox";
+       }
+   };
    
  function validar($nome, $documento, $curso, $telefone, $email, $ie) {
     validarnome($nome);
-     
+    validardocumento($documento);
+    validartelefone($telefone);
+    validaremail($email);
+    validarie($ie);
+    
+    require_once 'alunopdao.php';
+    insert($nome, $documento, $telefone, $email, $ie, $curso);
     
  };
  
+?>
