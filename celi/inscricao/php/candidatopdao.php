4c16d887e0d91a465061522818bb68e0e62d2a70
vinisiqueira00 <vinisiqueira2000@gmail.com>
2018-03-10 01:03:06 -0300

    -> Correção de erro de sintaxe nos arquivos 'extrairvalidar.php', 'candidatopdao.php', 'scriptsqlceli.txt'; -> Correção do erro de insert no BD; -> Início do planejamento para o novo layout da página 'forminscricao.html'; -> Alteração no CSS e no HTML da página 'forminscricao.html';

--- celi/inscricao/php/candidatopdao.php
+++ celi/inscricao/php/candidatopdao.php
@@ -1,14 +1,20 @@
 <?php
-$conexao = null;
 
-function conexao(&$conexao){
-    $conexao = mysqli_connect("localhost", "root", "", "dbceli");
-    if (mysqli_connect_errno()) {
-        printf("Connect failed: %s\n", mysqli_connect_error());
-        exit();
-    }
-};
+       // Função para estabelecer conexão com o BD
+       function conexao(){
+              $conexao = mysqli_connect("localhost", "root", "", "dbceli");
+              if (!$conexao){
+                     echo "ERROR! failure to connect to the database.";
+                     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+                     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+                     exit();
+              }
+              return $conexao;
+       };
 
-function insert ($nome, $documento, $telefone, $email, $ie, $idcurso){
-    $query = mysqli_query("INSERT INTO candidato (nome, documento, telefone, email, ie, idcurso) VALUES ( '$nome', '$documento' ,  '$telefone', '$email', '$ie' , '$idcurso');") or die("Erro ao tentar cadastrar");
-};
+       // Função para adicionar candidato no BD
+       function insert ($conexao, $nome, $documento, $telefone, $email, $ie, $idcurso){
+              $query = mysqli_query($conexao, "INSERT INTO candidato (nome, documento, telefone, email, ie, idcurso) VALUES ( '$nome', '$documento' ,  '$telefone', '$email', '$ie' , '$idcurso');") or die("ERRO: Insert não concluido (".mysqli_error($conexao).")");
+       };
+
+?>
--- celi/inscricao/php/extrairvalidar.php
+++ celi/inscricao/php/extrairvalidar.php
@@ -1,121 +1,145 @@
-<?php 
-   $nome=$_POST['name'];
-   $documento=$_POST['document'];
-   $curso= $_POST['course'];
-   $telefone=$_POST['phone'];
-   $email=$_POST['email'];
-   $ie=$_POST['radio'];
-   
-   validar($nome, $documento, $curso, $telefone, $email, $ie);
- 
-   function validarnome($nome){
-       if( trim($nome)=="" ){
-           echo "ERRO vazio nome";
-       }
-       else{
-            $arraynome= str_split($nome);
-            $lengthnome= strlen($nome);
-            $erroespecial=0;
-            $erroletra=0;
-            for($i=0;$i<$lengthnome;$i++){
-                if(is_numeric ($arraynome[$i])){
-                    $erroletra=1;
-                }
-            }
-            for($i=0;$i<$lengthnome;$i++){
-                $caracasciicode=ord($arraynome[$i]);
-                if($caracasciicode==32 || ($caracasciicode>=65 && $caracasciicode<=90) || ($caracasciicode>=97 && $caracasciicode<=122) || ($caracasciicode>=128 && $caracasciicode<=155) || $caracasciicode>=157 || ($caracasciicode>=160 && $caracasciicode<=165)){
-                    
-                }
-                    else{
-                           $erroespecial=1;                 
-                    }
-                }
-                
-                if($erroletra==1){
-                    echo "Erro número  ";
-                }
-                if($erroespecial==1){
-                    echo "Caractere inválido nome";
-                }
-            }
-            
-       
-   };
-   function validardocumento($documento){
-       if( trim($documento)=="" ){
-           echo "Campo documento está vazio";
-       }
-       else{
-           $arraydocumento= str_split($documento);
-           $lengthdocumento= strlen($documento);
-           $erroespecial=0;
-           for($i=0;$i<$lengthdocumento;$i++){
-               $caracasciicode=ord($arraydocumento[$i]);
-               if($caracasciicode==32 || ($caracasciicode>=45 && $caracasciicode<=46) || ($caracasciicode>=48 && $caracasciicode<=57) || ($caracasciicode>=65 && $caracasciicode<=90) || ($caracasciicode>=97 && $caracasciicode<=122) || ($caracasciicode>=128 && $caracasciicode<=155) || $caracasciicode>=157 || ($caracasciicode>=160 && $caracasciicode<=165)){
-                   
-               }
-               else{
-                   $erroespecial=1;
-               }
-           }
-           if($erroespecial==1){
-               echo "Caractere inválido documento";
-           }
-       }
-   };
-   
-   function validartelefone($telefone){
-       if( trim($telefone)=="" ){
-           echo "ERRO vazio telefone";
-       }
-           else{
-               $arraytelefone= str_split($telefone);
-               $lengthtelefone= strlen($telefone);
-               $erroespecial=0;
-               for($i=0;$i<$lengthtelefone;$i++){
-                   $caracasciicode=ord($arraytelefone[$i]);
-                   if($caracasciicode==45 || $caracasciicode==32 || $caracasciicode>=40 && $caracasciicode<=41 ||$caracasciicode>=48 && $caracasciicode<=57){
-                       
-                   }
-                   else{
-                       $erroespecial=1;
-                   }
-               
-               }
-               if($erroespecial==1){
-                   echo "Caractere inválido telefone";
-               }
-           }
-   };
-   
-   function validaremail($email){
-       if( trim($email)=="" ){
-           echo "ERRO vazio email";
-       }
-      if(!(filter_var($email, FILTER_VALIDATE_EMAIL)))
-       {
-        echo 'Email incorreto';
-       }
-   };
-   
-   function validarie ($ie){
-       if(!isset($ie)){
+<?php
+       // Chamando o arquivo .php pdao
+       require_once 'candidatopdao.php';
 
-        echo "ERRO vazio checkbox";
-       }
-   };
-   
- function validar($nome, $documento, $curso, $telefone, $email, $ie) {
-    validarnome($nome);
-    validardocumento($documento);
-    validartelefone($telefone);
-    validaremail($email);
-    validarie($ie);
-    
-    require_once 'candidatopdao.php';
-    insert($nome, $documento, $telefone, $email, $ie, $curso);
-    
- };
- 
+       // Pegando os dados do form
+       $nome=$_POST['name'];
+       $documento=$_POST['document'];
+       $curso= $_POST['course'];
+       $telefone=$_POST['phone'];
+       $email=$_POST['email'];
+       $ie=$_POST['radio'];
+
+       // Chamando a função que validar (ela chama todas as outras funções validadoras)
+       validar($nome, $documento, $curso, $telefone, $email, $ie);
+
+       // Função para validar o nome
+       function validarnome($nome){
+              $erronome = 0;
+              if( trim($nome)=="" ){
+                     $erronome = 1;
+                     echo "ERRO: campo 'nome' está vazio<br/>";
+              }
+              else{
+                     $arraynome= str_split($nome);
+                     $lengthnome= strlen($nome);
+                     $erroespecial=0;
+                     $erroletra=0;
+                     for($i=0;$i<$lengthnome;$i++){
+                            if(is_numeric ($arraynome[$i])){
+                                   $erroletra=1;
+                            }
+                     }
+                     for($i=0;$i<$lengthnome;$i++){
+                            $caracasciicode=ord($arraynome[$i]);
+                            if($caracasciicode==32 || ($caracasciicode>=65 && $caracasciicode<=90) || ($caracasciicode>=97 && $caracasciicode<=122) || ($caracasciicode>=128 && $caracasciicode<=155) || $caracasciicode>=157 || ($caracasciicode>=160 && $caracasciicode<=165)){}
+                            else{
+                                   $erroespecial=1;
+                            }
+                     }
+                     if($erroletra==1){
+                            $erronome = 1;
+                            echo "ERRO: contém números no campo 'nome'<br/>";
+                     }
+                     if($erroespecial==1){
+                            $erronome = 1;
+                            echo "ERRO: contém caractere especial no campo 'nome'<br/>";
+                     }
+              }
+              return $erronome;
+       };
+
+       // Função para validar o documento
+       function validardocumento($documento){
+              $errodocumento = 0;
+              if( trim($documento)=="" ){
+                     $errodocumento = 1;
+                     echo "ERRO: campo 'documento' está vazio<br/>";
+              }
+              else{
+                     $arraydocumento= str_split($documento);
+                     $lengthdocumento= strlen($documento);
+                     $erroespecial=0;
+                     for($i=0;$i<$lengthdocumento;$i++){
+                            $caracasciicode=ord($arraydocumento[$i]);
+                            if($caracasciicode==32 || ($caracasciicode>=45 && $caracasciicode<=46) || ($caracasciicode>=48 && $caracasciicode<=57) || ($caracasciicode>=65 && $caracasciicode<=90) || ($caracasciicode>=97 && $caracasciicode<=122) || ($caracasciicode>=128 && $caracasciicode<=155) || $caracasciicode>=157 || ($caracasciicode>=160 && $caracasciicode<=165)){}
+                            else{
+                                   $erroespecial=1;
+                            }
+                     }
+                     if($erroespecial==1){
+                            $errodocumento = 1;
+                            echo "ERRO: contém caractere especial no campo 'documento'<br/>";
+                     }
+              }
+              return $errodocumento;
+       };
+
+       // Função para validar o telefone
+       function validartelefone($telefone){
+              $errotelefone = 0;
+              if( trim($telefone)=="" ){
+                     $errotelefone = 1;
+                     echo "ERRO: campo 'telefone' está vazio<br/>";
+              }
+              else{
+                     $arraytelefone= str_split($telefone);
+                     $lengthtelefone= strlen($telefone);
+                     $erroespecial=0;
+                     for($i=0;$i<$lengthtelefone;$i++){
+                            $caracasciicode=ord($arraytelefone[$i]);
+                            if($caracasciicode==45 || $caracasciicode==32 || $caracasciicode>=40 && $caracasciicode<=41 ||$caracasciicode>=48 && $caracasciicode<=57){}
+                            else{
+                                   $erroespecial=1;
+                            }
+                     }
+                     if($erroespecial==1){
+                            $errotelefone = 1;
+                            echo "ERRO: contém caractere especias no campo 'telefone'<br/>";
+                     }
+              }
+              return $errotelefone;
+       };
+
+       // Função para validar o email
+       function validaremail($email){
+              $erroemail = 0;
+              if( trim($email)=="" ){
+                     $erroemail = 1;
+                     echo "ERRO: campo 'email' está vazio<br/>";
+              }
+              if(!(filter_var($email, FILTER_VALIDATE_EMAIL))){
+                     $erroemail = 1;
+                     echo "ERRO: campo 'email' invalido<br/>";
+              }
+              return $erroemail;
+       };
+
+       // Função para validar a situação
+       function validarie ($ie){
+              $errosituacao = 0;
+              if(!isset($ie)){
+                     $errosituacao = 1;
+                     echo "ERRO: campo 'situação' não marcado<br/>";
+              }
+              return $errosituacao;
+       };
+
+       // Função que dispara todas as outras funções e, estando tudo certo, inseri no BD
+       function validar($nome, $documento, $curso, $telefone, $email, $ie) {
+              $validnome = validarnome($nome);
+              $validdocumento = validardocumento($documento);
+              $validtelefone = validartelefone($telefone);
+              $validemail = validaremail($email);
+              $validsituacao = validarie($ie);
+
+              if($validnome == 0 && $validdocumento == 0 && $validtelefone == 0 && $validemail == 0 && $validsituacao == 0){
+
+                     $conexao = conexao();
+                     insert($conexao, $nome, $documento, $telefone, $email, $ie, $curso);
+
+              }
+
+       };
+
 ?>
