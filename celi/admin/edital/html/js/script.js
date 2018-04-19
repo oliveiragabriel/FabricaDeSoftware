a1f84cb433fc50874b19643ac3ab16c41610e22e
jvjsasam <josevictorjasam@yahoo.com.br>
2018-04-19 17:52:27 -0300

    Aterações no JQuery do inserir informações do Edital

--- celi/admin/edital/html/js/script.js
+++ celi/admin/edital/html/js/script.js
@@ -1,4 +1,10 @@
 $(function(){
+	$(".main-form-inputButton").on("submit", function(){
+		var vldDataAbertura =sdsd;
+		var vldDataEncerramento =ddsd;
+
+	});
+	
 	$(".main-form-checkbox").click(function(){
 		if($(this).is(':checked')){
 			var vagasInt = $(this).parent(".main-form-box-col1").next(".main-form-box-col2").find(".main-form-inputVagasInt");
@@ -13,4 +19,145 @@ $(function(){
 			vagasExt.prop("disabled", true);
 		}
 	});
+	
+	function validaDataAbertura(){
+		var dataCompleta = $(".main-form-inputDataAbert").val();
+		var arrayData = dataCompleta.split("-");
+		var anoAbertura = arrayData[0];
+		var mesAbertura = arrayData[1];
+		var diaAbertura = arrayData[2];
+		
+		if(mesAbertura < 1|| mesAbertura > 12){
+			var erro = 1;
+		}
+		
+		else if (diaAbertura < 1|| diaAbertura > 31){
+			var erro = 1;
+		}
+	};
+
+	function validaDataEncerramento(){
+		var dataCompleta = $(".main-form-inputDataEncer").val();
+		var arrayData = dataCompleta.split("-");
+		var anoEncerramento = arrayData[0];
+		var mesEncerramento = arrayData[1];
+		var diaEncerramento = arrayData[2];
+		
+		if(mesEncerramento < 1|| mesEncerramento > 12){
+			var erro = 1;
+		}
+		
+		else if (diaEncerramento < 1|| diaEncerramento > 31){
+			var erro = 1;
+		}
+	};
+	
+	function validaHorarioAbertura(){
+		var horarioCompleto = $(".main-form-inputHoraAbert").val();
+		var arrayHorario = horaCompleta.split(":");
+		var horaAbertura = arrayHorario[0];
+		var minutosAbertura = arrayHorario[1];
+		var segundosAbertura = arrayHorario[2];
+		
+		if (horaAbertura < 0|| horaAbertura > 24){
+			var erro = 1;
+		}
+		else if (minutoAbertura < 0|| minutoAbertura > 59){
+			var erro = 1;
+		}
+		else if (segundosAbertura < 0|| segundosAbertura > 59){
+			var erro = 1;
+		}
+	};
+	
+	function validaHorarioEncerramento(){
+		var horarioCompleto = $(".main-form-inputHoraEncer").val();
+		var arrayHorario = horaCompleta.split(":");
+		var horaEncerramento = arrayHorario[0];
+		var minutosEncerramento= arrayHorario[1];
+		var segundosEncerramento = arrayHorario[2];
+		
+		if (horaEncerramento < 0|| horaEncerramento > 24){
+			var erro = 1;
+		}
+		else if (minutoEncerramento < 0|| minutoEncerramento > 59){
+			var erro = 1;
+		}
+		else if (segundosEncerramento < 0|| segundosEncerramento > 59){
+			var erro = 1;
+		}
+	};
+	
+	function validaRelacaoAberturaEncerramento(){
+		var dataCompletaAbertura = $(".main-form-inputDataAbert").val();
+		var arrayDataAbertura = dataCompletaAbertura.split("-");
+		var anoAbertura = arrayDataAbertura[0];
+		var mesAbertura = arrayDataAbertura[1];
+		var diaAbertura = arrayDataAbertura[2];
+		var dataCompletaEncerramento = $(".main-form-inputDataEncer").val();
+		var arrayDataEncerramento = dataCompletaEncerramento.split("-");
+		var anoEncerramento = arrayDataEncerramento[0];
+		var mesEncerramento = arrayDataEncerramento[1];
+		var diaEncerramento = arrayDataEncerramento[2];
+		var horarioCompletoAbertura = $(".main-form-inputHoraAbert").val();
+		var arrayHorarioAbertura = horarioCompletoAbertura.split(":");
+		var horaAbertura = arrayHoraAbertura[0];
+		var minutosAbertura = arrayHoraAbertura[1];
+		var segundosAbertura = arrayHoraAbertura[2];
+		var horarioCompletoEncerramento = $(".main-form-inputHoraEncer").val();
+		var arrayHorarioEncerramento = horarioCompletoAbertura.split(":");
+		var horaEncerramento = arrayHorarioEncerramento[0];
+		var minutosEncerramento = arrayHorarioEncerramento[1];
+		var segundosEncerramento = arrayHorarioEncerramento[2];
+		
+		  if(anoAbertura < anoEncerramento){
+	        // Datas coerentes!
+	        var erro = 0;
+	      }
+	      else if(anoAbertura  == anoEncerramento){
+	        if(mesAbertura < mesEncerramento){
+	          // Datas coerentes!
+	        	var erro = 0;
+	        }
+	        else if(mesAbertura == mesEncerramento){
+	          if(diaAbertura < diaEncerramento){
+	            // Datas coerentes!
+	        	  var erro = 0;
+	          }
+	          else if(diaAbertura == diaEncerramento){
+	            if(horaAbertura < horaEncerramento){
+	              // Datas coerentes!
+	            	var erro = 0;
+	            }
+	            else if(horaAbertura == horaEncerramento){
+	              if(minutosAbertura < minutosEncerramento){
+	                // Datas coerentes!
+	            	  var erro = 0;
+	              }
+	              else{
+	                // Datas incoerentes!
+	            	  var erro = 1;
+	              }
+	            }
+	            else{
+	              // Datas incoerentes!
+	            	var erro = 1;
+	            }
+	          }
+	          else{
+	            // Datas incoerentes!
+	        	  var erro = 1;
+	          }
+	        }
+	        else{
+	          // Datas incoerentes!
+	        	var erro = 1;
+	        }
+	      }
+	      else{
+	        // Datas incoerentes!
+	         var erro = 1;
+	      }
+	      return erro;
+	    }
 });
