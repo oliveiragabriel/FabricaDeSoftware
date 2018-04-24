9268714df00beb413507ba7b594fc17b55d44408
Fábrica de software <Fbrica de software@Admin-HP>
2018-04-24 17:15:14 -0300

    sessao

--- celi/admin/login/control/loginAdmin.php
+++ celi/admin/login/control/loginAdmin.php
@@ -1,27 +1,30 @@
  <?php
-   
-    include_once"loginAdmin.html";
-    include_once "pdaoscript.php";
-    $usuarioDigitado=$_POST['nameUsuario'];
-    $senhaDigitado=$_POST['nameSenha'];
-    $conexao='';
+ 
+    require_once './cripAdmin.php';
+    include_once "../html/loginAdmin.html";
+    include_once "../../arquivosfixos/pdao/pdaoscript.php";
     
-    require_once'cripAdmin.php';
-    criptografar ($senhaDigitado);
+    $usuarioDigitado = $_POST['nameUsuario'];
+    $senhaDigitado = $_POST['nameSenha'];
     
-  conexao($conexao);
-   $sql="SELECT * FROM admin";
-   $query=mysqli_query($conexao, $sql);
-   
-   $array=mysqli_fetch_array($result_query);
-   $usuarioBd=$array['usuario'];
-   $senhaBd=$array['senha'];
+    criptografar($senhaDigitado);
+    
+    $select = selecionarbd("*", "admin", null);
+    $array = mysqli_fetch_array($result_query);
+    
+    $usuarioBd = $array['usuario'];
+    $senhaBd = $array['senha'];
 
-   if(($senhaBd==$senhaDigitado) && ($usuarioBd==$usuarioDigitado)){
-        echo "Bem vindo!";
-       header('paginaAdmin.html');
-     
-   }else{
-       echo "Inválido!";
-   }
+    if( $senhaBd == $senhaDigitado && $usuarioBd == $usuarioDigitado){
+       session_start();
+       
+       $_SESSION['usuario'] = $usuarioBd;
+       $_SESSION['senha'] = $senhaBd;
+       
+       
+       header('location: paginaAdmin.html');
+    }
+    else{
+       echo "Nome de usuário ou senha incorretos!";
+    }
 ?>
