41a50a13988611c28f747ba75ba06454c5471b4a
sthefanySilva <sthefany1@hotmail.com>
2018-04-24 17:42:29 -0300

    Add files via upload

---
+++ celi/admin/edital/html/listarEdital.php
@@ -0,0 +1,150 @@
+
+<!DOCTYPE html>
+<html lang="pt">
+	<head>
+        <meta charset="utf-8">
+        <title>Registrar Edital</title>
+        <!-- ESTILO da página -->
+        <link rel="stylesheet" type="text/css"
+        	href="../../../arquivosfixos/css/reset.css">
+        <link rel="stylesheet" type="text/css" href="./css/style.css">
+        <!-- ESTILO do header e do footer -->
+        <link rel="stylesheet" type="text/css"
+        	href="../../../arquivosfixos/css/header/style.css">
+        <link rel="stylesheet" type="text/css"
+        	href="../../../arquivosfixos/css/footer/style.css">
+       
+    </head>
+<body>
+
+	<div class="content">
+		<header id="header">
+			<div class="header-content">
+				<div class="header-logotipo">
+					<img class="header-logotipo-img"
+						src="../../../arquivosfixos/midia/logo-white2.png" alt="logotipo">
+				</div>
+				<div class="header-menuIcon">
+					<span class="header-menuIcon-content"></span>
+					<p class="header-menuIcon-legenda">menu</p>
+				</div>
+			</div>
+			<div class="header-menu">
+				<nav class="header-nav">
+					<ul class="header-list">
+						<a class="header-menuItem" href="#"> <span
+							class="header-menuItem-content header-menuItem-icon">+</span>
+							<p class="header-menuItem-content header-menuItem-text">Início</p>
+						</a>
+						<a class="header-menuItem" href="#"> <span
+							class="header-menuItem-content header-menuItem-icon">+</span>
+							<p class="header-menuItem-content header-menuItem-text">Edital</p>
+						</a>
+						<a class="header-menuItem"
+							href="../../cadastrarcurso/html/cadastrarcurso.html"> <span
+							class="header-menuItem-content header-menuItem-icon">+</span>
+							<p class="header-menuItem-content header-menuItem-text">Cursos</p>
+						</a>
+						<a class="header-menuItem"
+							href="../../../inscricao/html/inscricao.html"> <span
+							class="header-menuItem-content header-menuItem-icon">+</span>
+							<p class="header-menuItem-content header-menuItem-text">Inscrições</p>
+						</a>
+						<a class="header-menuItem" href="#"> <span
+							class="header-menuItem-content header-menuItem-icon">+</span>
+							<p class="header-menuItem-content header-menuItem-text">Sorteio</p>
+						</a>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<main id="main">
+		<div class="main-content">
+			<h1 class="main-title">Listar Edital</h1>
+			<table>
+				<th>
+					<td>#</td>
+					<td>Início</td>
+					<td>Término</td>
+					<td>Link</td>
+				</th>
+				
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</table>
+			
+			
+		</div>
+		</main>
+		<!-- FOOTER -->
+		<footer id="footer">
+			<div class="footer-developer">
+				<div class="footer-content">
+					<div class="footer-content-cefet">
+						<div class="footer-content-cefet-container">
+							<div class="footer-content-cefet-imagotipo">
+								<img class="footer-content-cefet-imagotipo-isotipo"
+									src="../../../arquivosfixos/midia/logocefet.png"
+									alt="logotipo do cefet">
+							</div>
+							<div class="footer-content-cefet-contato">
+								<div class="footer-content-cefet-contato-endereco">
+									<div class="footer-content-cefet-contato-endereco-icon">
+										<img class="footer-content-cefet-contato-endereco-icon-ctt"
+											src="../../../arquivosfixos/midia/endereço-icon.png"
+											alt="icon">
+									</div>
+									<div class="footer-content-cefet-contato-endereco-text">
+										<p class="footer-content-cefet-contato-endereco-text-ctt">Av.
+											Gov. Roberto Silveira, 1900 - Prado, Nova Friburgo - RJ,
+											28635-080</p>
+									</div>
+									<div style="clear: both;"></div>
+								</div>
+								<div class="footer-content-cefet-contato-telefone">
+									<div class="footer-content-cefet-contato-telefone-icon">
+										<img class="footer-content-cefet-contato-telefone-icon-ctt"
+											src="../../../arquivosfixos/midia/telefone-icon.png"
+											alt="icon">
+									</div>
+									<div class="footer-content-cefet-contato-telefone-text">
+										<p class="footer-content-cefet-contato-telefone-text-ctt">(22)
+											2519-8905</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="footer-content-fabricadesoftware">
+						<div class="footer-content-fabricadesoftware-container">
+							<div class="footer-content-fabricadesoftware-imagotipo">
+								<img class="footer-content-fabricadesoftware-imagotipo-isotipo"
+									src="../../../../docs/midia/FabricaDeSoftware-completo-(Light).png"
+									alt="logotipo da fábrica de software">
+								<div style="clear: both;"></div>
+							</div>
+							<div class="footer-content-fabricadesoftware-descricao">
+								<div class="footer-content-fabricadesoftware-descricao-text">
+									<p class="footer-content-fabricadesoftware-descricao-text-ctt">Lorem
+										ipsum dolor sit amet, consectetur adipisicing elit, sed do
+										eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+										enim ad minim veniam.</p>
+								</div>
+							</div>
+							<div style="clear: both;"></div>
+						</div>
+					</div>
+					<div style="clear: both;"></div>
+				</div>
+			</div>
+			<div class="creditosWP">
+				<p class="creditosWP-text">Créditos do Wordpress</p>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
